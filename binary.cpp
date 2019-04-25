110c2a1950f27c916649f7f7625c24d0dc03b0c1
viratabdkohli <49987637+viratabdkohli@users.noreply.github.com>
2019-04-26 01:04:20 +0530

    Update binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -1,104 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt = 0;
+void sort_arrary(int arr[],int n)
+{
+	int i,j,temp;
+	for(i=0;i<n-1;i++)
+		for(j=i+1;i<n;i++)
+			if(arr[i]>arr[j])
+			{
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+}
 
-void sortArray(int arr[], int n);
-void printArray(int arr[], int n);
-int  binary_search(int arr[], int n, int key);
+int binary_search(int arr[],int n,int key)
+{
+	int low=0,high=n-1,mid,count;
+
+	while(low<=high)
+	{
+		count++;
+		mid=(high+low)/2;
+
+		if(arr[mid]==key)
+			break;
+		else if(arr[mid]<key)
+			low=mid+1;
+		else
+			high=mid-1;
+	}
+	return count;
+}
 
 int main()
 {
-	/*
-	int testcase;
-	cout << "Enter the number of testcases" << endl;
-	cin >>  testcase;
+	int t,n=0;
+	cout<<"Enter the num of testcases\n";
+	cin>>t;
 
-	int n = 0;
+	ofstream worst("worst.dat",ios::trunc);
+	ofstream avg("avg.dat",ios::trunc);
+	ofstream best("best.dat",ios::trunc);
 
-	ofstream avg("average.dat", ios::trunc);
-	ofstream worst("worst.dat", ios::trunc);
-	ofstream best("best.dat", ios::trunc);
-	while(testcase--)
+	while(t--)
 	{
-		n += 10;
-
+		n+=10;
 		int arr[n];
 
 		srand(time(0));
 
-		for(int index = 0; index < n; index++)
-			arr[index] = rand() % n;
+		for(int i=0;i<n;i++)
+			arr[i]=rand()%n;
+		sort_arrary(arr,n);
+		int key = rand()%(n/2);
 
-		sort(arr, arr+n);
-
-		int key = rand() % (n/2);
-		cnt = 0;
-		binary_search(arr, n, key);
-		avg << n  << " " << cnt << endl;
-
-		cnt = 0;
-		binary_search(arr, n, arr[(n-1)/2]);
-		best << n  << " " << cnt << endl;
-
-		cnt = 0;
-		binary_search(arr, n, n);
-		worst << n  << " " << cnt << endl;
-	}
-	*/
-
-	int n;
-	cout << "Enter the number of elements" << endl;
-	cin >> n;
-
-	int arr[n];
-	cout << "Enter array elements" << endl;
-	for(int i = 0; i < n; i++)
-	    cin >> arr[i];
-
-	int key;
-	cout << "Enter a key to be searched" << endl;
-	cin >> key;
-
-	int result = binary_search(arr, n, key);
-
-	if(result == -1)
-		cout << "Key is not present" << endl;
-	else
-		cout << "Key is present at location " << result << endl;
-
+		avg<<n<<" "<<binary_search(arr,n,key);
+		worst<<n<<" "<<binary_search(arr,n,n);
+		best<<n<<" "<<binary_search(arr,n,arr[(n-1)/2]);
+	}	
 	return 0;
 }
-
-void printArray(int arr[], int n)
-{
-	cout << "Array elements are" << endl;
-	int i;
-	for(i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-int  binary_search(int arr[], int n, int key)
-{
-	int low  = 0;
-	int high = n - 1;
-	int mid;
-
-	while(low <= high)
-	{
-		cnt++;
-
-		mid = (low + high) / 2;
-
-		if(arr[mid] == key)
-			return mid;
-		else if(arr[mid] < key)
-			low = mid + 1;
-		else
-			high = mid - 1;
-	}
-
-	return -1;
-}
-
